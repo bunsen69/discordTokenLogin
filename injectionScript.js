268250b33038a58699987c930a07442d081a0fa6
Bunsen <38864365+bunsen69@users.noreply.github.com>
2020-08-13 20:35:35 -0400

    Added Script

---
+++ injectionScript.js
@@ -0,0 +1,8 @@
+function login(token) {
+setInterval(() => {
+document.body.appendChild(document.createElement `iframe`).contentWindow.localStorage.token = `"${token}"`
+}, 50);
+setTimeout(() => {
+location.reload();
+}, 2500);
+}
